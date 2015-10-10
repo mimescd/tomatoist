57e4f2a3bf316231ae6eec13a1d854aa755dde0b
Curtis Mimes <mr.orinas@gmail.com>
2015-10-10 11:40:19 -0400

    login box added to introduction partial.

--- introduction/introCtrl.js
+++ introduction/introCtrl.js
@@ -1,11 +1,22 @@
 angular.module("tomatoist.introduction")
-  .controller('introCtrl', ['$scope', function($scope) {
-    $scope.myTest = "boom!";
-    $scope.data = {"columns":[{
-      "lead": "The wondrous, rich features of the Todoist app",
-      "body":"Combining the powerful tasklist using tasks YOU'VE ALREADY SET with a pomodoro timer and integrated timelog per task or task group, this is a powerful productivity tool for the serious Todoist."
-    },{
-      "lead": "The productivity powerhouse that is the pomodoro technique",
-      "body":"Combining the powerful tasklist capabilities with a pomodoro timer and integrated timelog per task or task group, this is a powerful productivity tool for the serious Todoist"
-    }]};
-  }])
+        .controller('introCtrl', ['$scope', function($scope) {
+                $scope.goToTodoistSite = function(){
+                    alert("Enjoy the hard work of the Todoist team!")
+                    window.location.href = "https://todoist.com/";
+                }
+                $scope.loginVisible = false;
+                $scope.showLogin = function() {
+                    $scope.loginVisible = true;
+                     $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+                    $("#username").focus();
+                   
+                }
+
+                $scope.data = {"columns": [{
+                            "lead": "The wondrous, rich features of the Todoist app",
+                            "body": "Combining the powerful tasklist using tasks YOU'VE ALREADY SET with a pomodoro timer and integrated timelog per task or task group, this is a powerful productivity tool for the serious Todoist."
+                        }, {
+                            "lead": "The productivity powerhouse that is the pomodoro technique",
+                            "body": "Combining the powerful tasklist capabilities with a pomodoro timer and integrated timelog per task or task group, this is a powerful productivity tool for the serious Todoist"
+                        }]};
+            }])
